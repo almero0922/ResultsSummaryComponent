2b401b78f817944c4abe7b75d6f4647dcb36fbfa
almero0922 <albertomedel29@gmail.com>
2023-07-23 15:16:07 -0500

    Mobile Version Done

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-fetch('./data.json')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
 const cssColorsNames = ['--LightRed','--OrangeyYellow','--GreenTeal','--CobaltBlue'];
+const cssColorsNamesB = ['--LightRedB','--OrangeyYellowB','--GreenTealB','--CobaltBlueB'];
 const cssColorsValues = [];
+const cssColorsValuesB = [];
 cssColorsNames.forEach(element => {
     cssColorsValues.push(window.getComputedStyle(document.documentElement).getPropertyValue(element));
 });
-console.log(cssColorsValues);
+cssColorsNamesB.forEach(element => {
+    cssColorsValuesB.push(window.getComputedStyle(document.documentElement).getPropertyValue(element));
+});
+
+const sumContainers = document.getElementsByClassName('summary-container-ind');
+console.log(sumContainers);
+/*sumContainers.forEach((element, index) => {
+    element.style.backgroundColor = cssColorsValues[index];
+});*/
+fetch('./data.json')
+    .then((response) => response.json())
+    .then((json) => {
+        for(var i = 0; i < cssColorsValues.length; i++){
+            sumContainers[i].style.backgroundColor = cssColorsValuesB[i];
+            //let img = 'url('+json[i].icon+')';
+            sumContainers[i].children[0].children[0].src = json[i].icon;
+            sumContainers[i].children[0].children[1].innerText = json[i].category;
+            sumContainers[i].children[0].children[1].style.color = cssColorsValues[i];
+            sumContainers[i].children[1].children[0].innerText = json[i].score;
+            sumContainers[i].children[1].children[0].style.marginRight = 'o.8rem';
+        }
+    })
